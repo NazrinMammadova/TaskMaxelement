af4bb99021c9d93f0c428127e8b1220a5dfffb35
NazrinMammadova <102550859+NazrinMammadova@users.noreply.github.com>
2022-04-19 04:23:27 -0700

    i hAVE DONE MY TASK :D

---
+++ tast1april19/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace tast1april19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+            int[] numbers = { 1, 55, 190, 100 };
+            int maxNumber = numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > maxNumber)
+                {
+                    maxNumber = numbers[i];
+                }
+
+            }
+            Console.WriteLine(maxNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
